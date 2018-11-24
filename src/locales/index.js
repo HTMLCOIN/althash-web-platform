18d05a63960aa66d75c4139b3af4507b00e6d275
Jaron Rosenau <jaron.rosenau@gmail.com>
2018-11-23 23:54:11 -0800

    add portuguese and dutch

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,14 +1,20 @@
 import en from './en'
+import br from './br'
+import nl from './nl'
 import zh from './zh'
 import ko from './ko'
 
 const locales = [
   'en',
+  'br',
+  'nl',
   'zh',
   'ko'
 ]
 const messages = {
   en,
+  br,
+  nl,
   zh,
   ko
 }
